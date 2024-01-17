4debc2f4ebad6489546950d0dc70d6e34b5915c1
Luke <lsimpsonv1@gmail.com>
2024-01-16 20:25:28 -0600

    add server and test route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::net::SocketAddr;
+
+use axum::routing::get;
+use axum::Router;
+
+async fn test() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let app = Router::new().route("/api/test", get(test));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on: {}", addr);
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    axum::serve(listener, app).await?;
+    Ok(())
 }
