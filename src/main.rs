2786bb4e79754653509a12170a535c9c3adbc764
Luke <lsimpsonv1@gmail.com>
2024-01-16 22:27:15 -0600

    add post handler

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn test() -> &'static str {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let app = Router::new().route("/api/test", get(test));
+    let app = Router::new().route("/api/test", get(test).post(test));
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("Listening on: {}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await?;
